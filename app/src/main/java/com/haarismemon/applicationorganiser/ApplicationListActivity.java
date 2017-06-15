1860f0611858eb3c05581b3c31fe6c02d08a0d29
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-15 20:43:16 +0100

    Use DataSource in ApplicationListActivity to open and close database

--- app/src/main/java/com/haarismemon/applicationorganiser/ApplicationListActivity.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/ApplicationListActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.haarismemon.applicationorganiser.database.DataSource;
+
 public class ApplicationListActivity extends AppCompatActivity {
+
+    DataSource mDataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,19 @@ public class ApplicationListActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
 
+        mDataSource = new DataSource(this);
+        mDataSource.open();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDataSource.close();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDataSource.open();
+    }
 }
