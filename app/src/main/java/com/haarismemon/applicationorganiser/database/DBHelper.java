61524ff8895bccc1493c49e3724e68126ce8c27b
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-15 20:11:29 +0100

    Add a Database helper class to use when created or upgraded

---
+++ app/src/main/java/com/haarismemon/applicationorganiser/database/DBHelper.java
@@ -0,0 +1,32 @@
+package com.haarismemon.applicationorganiser.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.haarismemon.applicationorganiser.model.Internship;
+
+import static android.R.attr.version;
+
+/**
+ * Created by Haaris on 15/06/2017.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_FILE_NAME = "applications.db";
+    public static final int DB_VERSION = 1;
+
+
+    public DBHelper(Context context) {
+        super(context, DB_FILE_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+}
