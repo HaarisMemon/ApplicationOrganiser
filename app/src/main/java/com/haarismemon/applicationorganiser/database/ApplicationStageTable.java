ed22dbb09ac62edc44f1b4e1a10c210f9fd53c66
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-15 20:10:02 +0100

    Add an Application Stage database table information class

---
+++ app/src/main/java/com/haarismemon/applicationorganiser/database/ApplicationStageTable.java
@@ -0,0 +1,46 @@
+package com.haarismemon.applicationorganiser.database;
+
+/**
+ * Created by Haaris on 15/06/2017.
+ */
+
+public class ApplicationStageTable {
+
+    public static final String TABLE_APPLICATION_STAGE = "application_stage";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_STAGE_NAME = "stage_name";
+    public static final String COLUMN_IS_COMPLETED = "is_completed";
+    public static final String COLUMN_IS_WAITING = "is_waiting";
+    public static final String COLUMN_IS_SUCCESSFUL = "is_successful";
+    public static final String COLUMN_START_DATE = "start_date";
+    public static final String COLUMN_COMPLETE_DATE = "complete_date";
+    public static final String COLUMN_REPLY_DATE = "reply_date";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_CREATED_ON = "created_on";
+    public static final String COLUMN_MODIFIED_ON = "modified_on";
+    public static final String COLUMN_INTERNSHIP_ID = "internship_id";
+
+
+    public static final String SQL_CREATE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_APPLICATION_STAGE +
+                    " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_STAGE_NAME + " VARCHAR, " +
+                    COLUMN_IS_COMPLETED + " INTEGER DEFAULT 0, " +
+                    COLUMN_IS_WAITING + " INTEGER DEFAULT 0, " +
+                    COLUMN_IS_SUCCESSFUL + " INTEGER DEFAULT 0, " +
+                    //TEXT as ISO8601 strings ("YYYY-MM-DD HH:MM:SS.SSS").
+                    COLUMN_START_DATE + " VARCHAR, " +
+                    COLUMN_COMPLETE_DATE + " DATETIME, " +
+                    COLUMN_REPLY_DATE + " DATETIME, " +
+                    COLUMN_DESCRIPTION + " DATETIME, " +
+                    COLUMN_CREATED_ON + " DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+                    COLUMN_MODIFIED_ON + " DATETIME DEFAULT CURRENT_TIMESTAMP," +
+                    COLUMN_INTERNSHIP_ID + " INTEGER, " +
+                    "FOREIGN KEY(" + COLUMN_INTERNSHIP_ID + ") REFERENCES " + InternshipTable.TABLE_INTERNSHIP + "(" + InternshipTable.COLUMN_ID + ") " +
+                    ")";
+
+    public static final String SQL_DELETE =
+            "DROP TABLE " + TABLE_APPLICATION_STAGE;
+
+}
