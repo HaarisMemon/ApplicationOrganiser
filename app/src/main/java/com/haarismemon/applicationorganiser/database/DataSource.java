18cff77119f1bd3322d82f58b14a78161e0ee0bb
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-15 20:37:13 +0100

    Add DataSource class to manage Database

---
+++ app/src/main/java/com/haarismemon/applicationorganiser/database/DataSource.java
@@ -0,0 +1,26 @@
+package com.haarismemon.applicationorganiser.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.haarismemon.applicationorganiser.model.ApplicationStage;
+import com.haarismemon.applicationorganiser.model.Internship;
+
+/**
+ * Created by Haaris on 15/06/2017.
+ */
+
+public class DataSource {
+
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+    private DBHelper mDbHelper;
+
+    public DataSource(Context Context) {
+        this.mContext = Context;
+        mDbHelper = new DBHelper(mContext);
+        mDatabase = mDbHelper.getWritableDatabase();
+    }
+
+}
