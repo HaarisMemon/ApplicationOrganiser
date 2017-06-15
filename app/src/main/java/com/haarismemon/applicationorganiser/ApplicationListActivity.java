fee721a7c52b9832581229f9e299b4b6805861c4
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-15 22:37:50 +0100

    Add condition to only populate database if empty

--- app/src/main/java/com/haarismemon/applicationorganiser/ApplicationListActivity.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/ApplicationListActivity.java
@@ -23,20 +23,8 @@ public class ApplicationListActivity extends AppCompatActivity {
 
         mDataSource = new DataSource(this);
         mDataSource.open();
-
-        Internship ibm = new Internship("IBM", "Software Engineer Placement", "1 Year", "London");
-        Internship cognito = new Internship("Cognito iQ", "Software Developer Placement", "1 Year", "Newbury");
-
-        mDataSource.createInternship(ibm);
-        mDataSource.createInternship(cognito);
-
-        ApplicationStage onlineApplication = new ApplicationStage("Online Application", true, false, true,"28/01/2017","28/01/2017","20/02/2017",ibm.internshipId);
-        ApplicationStage test = new ApplicationStage("Test",true,false,true,null,null,"20/02/2017", ibm.internshipId);
-        ApplicationStage assessmentCentre = new ApplicationStage("Assessment Centre",true,false,true,"07/03/2017","07/03/2017","16/03/2017", ibm.internshipId);
-
-        mDataSource.createApplicationStage(onlineApplication);
-        mDataSource.createApplicationStage(test);
-        mDataSource.createApplicationStage(assessmentCentre);
+        mDataSource.seedDatbase();
+        
     }
 
     @Override
--- app/src/main/java/com/haarismemon/applicationorganiser/database/DataSource.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/database/DataSource.java
@@ -2,6 +2,7 @@ package com.haarismemon.applicationorganiser.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.haarismemon.applicationorganiser.model.ApplicationStage;
@@ -42,6 +43,32 @@ public class DataSource {
         ContentValues values = applicationStage.toValues();
         mDatabase.insert(ApplicationStageTable.TABLE_APPLICATION_STAGE, null, values);
         return applicationStage;
+    }
+
+    public void seedDatbase() {
+        long numInternships = DatabaseUtils.queryNumEntries(mDatabase, InternshipTable.TABLE_INTERNSHIP);
+        long numApplicationStageCount = DatabaseUtils.queryNumEntries(mDatabase, ApplicationStageTable.TABLE_APPLICATION_STAGE);
+
+        if(numInternships == 0) {
+            Internship ibm = new Internship("IBM", "Software Engineer Placement", "1 Year", "London");
+            Internship cognito = new Internship("Cognito iQ", "Software Developer Placement", "1 Year", "Newbury");
+
+            createInternship(ibm);
+            createInternship(cognito);
+
+            if(numApplicationStageCount == 0) {
+
+                ApplicationStage onlineApplication = new ApplicationStage("Online Application", true, false, true, "28/01/2017", "28/01/2017", "20/02/2017", ibm.internshipId);
+                ApplicationStage test = new ApplicationStage("Test", true, false, true, null, null, "20/02/2017", ibm.internshipId);
+                ApplicationStage assessmentCentre = new ApplicationStage("Assessment Centre", true, false, true, "07/03/2017", "07/03/2017", "16/03/2017", ibm.internshipId);
+
+                createApplicationStage(onlineApplication);
+                createApplicationStage(test);
+                createApplicationStage(assessmentCentre);
+
+            }
+        }
+
     }
 
 }
