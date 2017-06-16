2071c9bea4048e9b2f3ddf44ad38aa29f5fc3a01
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-16 12:11:56 +0100

    Display application stage information when clicked

--- app/src/main/java/com/haarismemon/applicationorganiser/InternshipInformationActivity.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/InternshipInformationActivity.java
@@ -3,11 +3,14 @@ package com.haarismemon.applicationorganiser;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.haarismemon.applicationorganiser.database.ApplicationStageTable;
 import com.haarismemon.applicationorganiser.database.DataSource;
 import com.haarismemon.applicationorganiser.database.InternshipTable;
 import com.haarismemon.applicationorganiser.model.ApplicationStage;
@@ -40,24 +43,28 @@ public class InternshipInformationActivity extends AppCompatActivity {
 
         companyNameText.setText(internship.getCompanyName());
         roleText.setText(internship.getRole());
-        lengthText.setText(internship.getLength());
-        locationText.setText(internship.getLocation());
-
-        if(internship.getDescription() != null) {
-            descriptionText.setText(internship.getDescription());
-        } else {
-            descriptionText.setText("No Description");
-        }
+        lengthText.setText(internship.getLength() != null ? internship.getLength() : "None");
+        locationText.setText(internship.getLocation() != null ? internship.getLocation() : "None");
+        descriptionText.setText(internship.getDescription() != null ? internship.getDescription() : "No Description");
 
 
         ListView applicationStageListView = (ListView) findViewById(R.id.applicationStageListView);
 
-        List<ApplicationStage> stages = mDataSource.getAllApplicationStages(internship.getInternshipId());
+        final List<ApplicationStage> stages = mDataSource.getAllApplicationStages(internship.getInternshipId());
 
         ArrayAdapter<ApplicationStage> arrayAdapter = new ArrayAdapter<ApplicationStage>(
                 getApplicationContext(), android.R.layout.simple_expandable_list_item_1, stages);
 
         applicationStageListView.setAdapter(arrayAdapter);
+
+        applicationStageListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), StageInformationActivity.class);
+                intent.putExtra(ApplicationStageTable.COLUMN_ID, stages.get(i).getStageID());
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/haarismemon/applicationorganiser/StageInformationActivity.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/StageInformationActivity.java
@@ -1,13 +1,47 @@
 package com.haarismemon.applicationorganiser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.haarismemon.applicationorganiser.database.ApplicationStageTable;
+import com.haarismemon.applicationorganiser.database.DataSource;
+import com.haarismemon.applicationorganiser.model.ApplicationStage;
 
 public class StageInformationActivity extends AppCompatActivity {
+
+    DataSource mDataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage_information);
+
+        mDataSource = new DataSource(this);
+
+        TextView stageNameText = (TextView) findViewById(R.id.stageNameText);
+        TextView isCompletedText = (TextView) findViewById(R.id.isCompletedText);
+        TextView isWaitingForResponseText = (TextView) findViewById(R.id.isWaitingForResponseText);
+        TextView isSuccessfulText = (TextView) findViewById(R.id.isSuccessfulText);
+        TextView dateOfStartText = (TextView) findViewById(R.id.dateOfStartText);
+        TextView dateOfCompletionText = (TextView) findViewById(R.id.dateOfCompletionText);
+        TextView dateOfReplyText = (TextView) findViewById(R.id.dateOfReplyText);
+        TextView stageDescriptionText = (TextView) findViewById(R.id.stageDescriptionText);
+
+
+        Intent intent = getIntent();
+
+        ApplicationStage stage = mDataSource.getApplicationStage(intent.getLongExtra(ApplicationStageTable.COLUMN_ID, -1));
+
+        stageNameText.setText(stage.getStageName());
+        isCompletedText.setText(stage.isCompleted() ? "Yes" : "No");
+        isWaitingForResponseText.setText(stage.isWaitingForResponse() ? "Yes" : "No");
+        isSuccessfulText.setText(stage.isSuccessful() ? "Yes" : "No");
+        dateOfStartText.setText(stage.getDateOfStart() != null ? stage.getDateOfStart() : "None");
+        dateOfCompletionText.setText(stage.getDateOfCompletion() != null ? stage.getDateOfCompletion() : "None");
+        dateOfReplyText.setText(stage.getDateOfReply() != null ? stage.getDateOfReply() : "None");
+        stageDescriptionText.setText(stage.getDescription() != null ? stage.getDescription() : "No Description");
+
     }
 }
