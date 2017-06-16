7b414d265c2ecaea75b773ef54e28949aef4ba89
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-16 11:41:03 +0100

    Add an activity to show Application Stage Information

---
+++ app/src/main/java/com/haarismemon/applicationorganiser/StageInformationActivity.java
@@ -0,0 +1,13 @@
+package com.haarismemon.applicationorganiser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StageInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage_information);
+    }
+}
