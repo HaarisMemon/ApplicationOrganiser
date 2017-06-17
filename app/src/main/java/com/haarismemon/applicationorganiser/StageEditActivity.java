d0d3466873f54efb9a5ed9bf1d3d29e850e10417
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-17 14:05:54 +0100

    Implement the date picking for the edit application stage

--- app/src/main/java/com/haarismemon/applicationorganiser/StageEditActivity.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/StageEditActivity.java
@@ -1,12 +1,25 @@
 package com.haarismemon.applicationorganiser;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.haarismemon.applicationorganiser.database.DataSource;
+
+import java.util.Calendar;
+
+import static com.haarismemon.applicationorganiser.R.id.descriptionEditText;
+import static com.haarismemon.applicationorganiser.R.id.lengthEditText;
+import static com.haarismemon.applicationorganiser.R.id.locationEditText;
 
 public class StageEditActivity extends AppCompatActivity {
 
@@ -15,12 +28,33 @@ public class StageEditActivity extends AppCompatActivity {
     private DataSource mDataSource;
     private boolean isEditMode;
 
+    DatePickerDialog.OnDateSetListener mDataSetListener;
+
+    Button dateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage_edit);
 
         mDataSource = new DataSource(this);
+        dateButton = (Button) findViewById(R.id.startDateButton);
+
+        mDataSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+
+                String monthOfYear = Integer.toString(month);
+                monthOfYear = (monthOfYear.length() == 1) ? monthOfYear = "0" + monthOfYear : monthOfYear;
+
+                String dayOfMonth = Integer.toString(day);
+                dayOfMonth = (dayOfMonth.length() == 1) ? dayOfMonth = "0" + dayOfMonth : dayOfMonth;
+
+                String date = dayOfMonth + "/" + monthOfYear + "/" + year;
+                dateButton.setText(date);
+            }
+        };
 
         if(isEditMode) {
             setTitle("Edit Stage");
@@ -56,6 +90,50 @@ public class StageEditActivity extends AppCompatActivity {
 
     private void saveStage() {
 
+    }
+
+    public void pickDate(View view) {
+        Log.i("buttonID", ""+view.getId());
+        Log.i("startDateButton", ""+R.id.startDateButton);
+        Log.i("completionDateButton", ""+R.id.completionDateButton);
+        Log.i("replyDateButton", ""+R.id.replyDateButton);
+
+        if(view.getId() == R.id.startDateButton) {
+            dateButton = (Button) findViewById(R.id.startDateButton);
+        } else if(view.getId() == R.id.completionDateButton) {
+            dateButton = (Button) findViewById(R.id.completionDateButton);
+        } else if(view.getId() == R.id.replyDateButton) {
+            dateButton = (Button) findViewById(R.id.replyDateButton);
+        } else {
+            dateButton = null;
+        }
+
+        switch (view.getId()) {
+            case R.id.startDateButton:
+                dateButton = (Button) findViewById(R.id.startDateButton);
+            case R.id.completionDateButton:
+                dateButton = (Button) findViewById(R.id.completionDateButton);
+            case R.id.replyDateButton:
+                dateButton = (Button) findViewById(R.id.replyDateButton);
+            default:
+                dateButton = null;
+        }
+
+        if(dateButton != null) {
+            Calendar cal = Calendar.getInstance();
+
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            int month = cal.get(Calendar.MONTH);
+            int year = cal.get(Calendar.YEAR);
+
+            DatePickerDialog dialog = new DatePickerDialog(StageEditActivity.this,
+                    android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    mDataSetListener, year, month, day);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Button not found", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
