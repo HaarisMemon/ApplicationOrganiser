b04f7339a4551ff80293cf3a27789739b55e66ee
Haaris Memon <haaris.memon@kcl.ac.uk>
2017-06-21 22:55:03 +0100

    Implement deleting multiple internships at once in Application List

--- app/src/main/java/com/haarismemon/applicationorganiser/ApplicationListActivity.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/ApplicationListActivity.java
@@ -3,6 +3,7 @@ package com.haarismemon.applicationorganiser;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -41,6 +42,7 @@ public class ApplicationListActivity extends AppCompatActivity {
     private ActionMode actionMode;
     private ActionMode.Callback actionModeCallback;
     private List<Internship> internships;
+    private Map<Internship, CardView> selectedInternshipCardMap;
 
     /**
      * RecyclerAdapter of RecyclerView for internships in the activity
@@ -74,6 +76,9 @@ public class ApplicationListActivity extends AppCompatActivity {
         //ArrayList of all internships in the database
         internships = mDataSource.getAllInternship();
 
+        //map to track which internships have been selected, and which card they are linked to
+        selectedInternshipCardMap = new HashMap<>();
+
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -105,6 +110,10 @@ public class ApplicationListActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     //when the delete action button is pressed in action mode
                     case R.id.action_mode_delete:
+                        //delete all selected internships
+                        deleteSelectedInternships();
+                        //exit action mode
+                        switchActionMode(false);
                         return true;
 
                 }
@@ -115,6 +124,8 @@ public class ApplicationListActivity extends AppCompatActivity {
             @Override
             public void onDestroyActionMode(ActionMode actionMode) {
                 isSelectionMode = false;
+                //unselect all selected internships
+                unselectSelectedInternships();
                 //update the recycler view
                 recyclerAdapter.notifyDataSetChanged();
             }
@@ -194,6 +205,103 @@ public class ApplicationListActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    /**
+     * Turns on action mode if true passed, otherwise turns action mode off
+     * @param turnOn true if to turn action mode on
+     */
+    public void switchActionMode(boolean turnOn) {
+        //if turnOn is true, then start action mode
+        if(turnOn) {
+            isSelectionMode = true;
+            actionMode = startActionMode(actionModeCallback);
+        } else {
+            //else if turnOn is false, then exit action mode
+            if (actionMode != null) {
+                isSelectionMode = false;
+                actionMode.finish();
+            }
+        }
+    }
+
+    //this method updates the title in the action bar in action mode, and is called every time internship selected
+    private void updateActionModeCounter(int counter) {
+        if(counter == 1) {
+            actionMode.setTitle(counter + " internship selected");
+        } else {
+            actionMode.setTitle(counter + " internships selected");
+        }
+    }
+
+    /**
+     * Selects or deselects the internship depending on value of toBeSelected,
+     * and updates the action bar counter and the card background
+     * If 0 internships are selected, then action mode turns off
+     * @param cardView that is linked to the internship in the RecyclerView
+     * @param internship that is to be selected or deselected
+     * @param toBeSelected true if the internship is to be selected
+     */
+    public void prepareSelection(CardView cardView, Internship internship, boolean toBeSelected) {
+        //update the internship being selected
+        internship.setSelected(toBeSelected);
+
+        //if to be selected put internship in selected map, and update the cardView background
+        if(toBeSelected) {
+            selectedInternshipCardMap.put(internship, cardView);
+            updateCardBackground(cardView, true);
+        } else {
+            selectedInternshipCardMap.remove(internship);
+            updateCardBackground(cardView, false);
+        }
+
+        //if user selects no internships then exit action mode
+        if(selectedInternshipCardMap.size() == 0) {
+            switchActionMode(false);
+        } else if(actionMode != null) {
+            updateActionModeCounter(selectedInternshipCardMap.size());
+        }
+
+    }
+
+    /**
+     * Update the color of the cardView background depending on whether it is selected or not
+     * @param cardView to change background of
+     * @param isSelected true if background to set is highlighted, otherwise set to original color
+     */
+    public void updateCardBackground(CardView cardView, boolean isSelected) {
+        if(isSelected) {
+            cardView.setCardBackgroundColor(ContextCompat.getColor(this, R.color.internshipCardBackgroundSelected));
+        } else {
+            cardView.setCardBackgroundColor(ContextCompat.getColor(this, R.color.internshipCardBackgroundDefault));
+        }
+    }
+
+    //unselects all the selected internships
+    private void unselectSelectedInternships() {
+        for(Internship internship : selectedInternshipCardMap.keySet()) {
+            internship.setSelected(false);
+            updateCardBackground(selectedInternshipCardMap.get(internship), false);
+        }
+
+        selectedInternshipCardMap.clear();
+    }
+
+    //deletes all selected internships
+    private void deleteSelectedInternships() {
+        //for all selected internships
+        for(Internship deleteInternship : selectedInternshipCardMap.keySet()) {
+            //remove from list
+            internships.remove(deleteInternship);
+            //delete from database
+            mDataSource.deleteInternship(deleteInternship.getInternshipID());
+        }
+        //update the RecyclerView through the adapter
+        recyclerAdapter.internshipsList = internships;
+        recyclerAdapter.notifyDataSetChanged();
+
+        //empty the map holding the selected internships
+        selectedInternshipCardMap.clear();
     }
 
 }
--- app/src/main/java/com/haarismemon/applicationorganiser/adapter/ApplicationListRecyclerAdapter.java
+++ app/src/main/java/com/haarismemon/applicationorganiser/adapter/ApplicationListRecyclerAdapter.java
@@ -1,12 +1,13 @@
 package com.haarismemon.applicationorganiser.adapter;
 
-import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.haarismemon.applicationorganiser.ApplicationListActivity;
 import com.haarismemon.applicationorganiser.InternshipInformationActivity;
 import com.haarismemon.applicationorganiser.R;
 import com.haarismemon.applicationorganiser.database.InternshipTable;
@@ -22,9 +23,9 @@ import java.util.List;
 public class ApplicationListRecyclerAdapter extends RecyclerView.Adapter<ApplicationListViewHolder> {
 
     public List<Internship> internshipsList;
-    private Context context;
+    private ApplicationListActivity context;
 
-    public ApplicationListRecyclerAdapter(Context context, List<Internship> internshipsList) {
+    public ApplicationListRecyclerAdapter(ApplicationListActivity context, List<Internship> internshipsList) {
         this.internshipsList = internshipsList;
         this.context = context;
     }
@@ -38,7 +39,7 @@ public class ApplicationListRecyclerAdapter extends RecyclerView.Adapter<Applica
     }
 
     @Override
-    public void onBindViewHolder(ApplicationListViewHolder holder, final int position) {
+    public void onBindViewHolder(final ApplicationListViewHolder holder, final int position) {
         final Internship internship = internshipsList.get(position);
         //adds the company name, role and last updated date to the cardView holder
         holder.companyName.setText(internship.getCompanyName());
@@ -49,12 +50,60 @@ public class ApplicationListRecyclerAdapter extends RecyclerView.Adapter<Applica
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, InternshipInformationActivity.class);
-                //send the ID of the Internship you want to see information of
-                intent.putExtra(InternshipTable.COLUMN_ID, internship.getInternshipID());
-                context.startActivity(intent);
+                //store view as card view so to access card view methods
+                CardView cardView = (CardView) view;
+
+                //if when single clicked the selection mode is already on
+                if(context.isSelectionMode) {
+                    //if internship was already selected
+                    if(internship.isSelected()) {
+                        //deselect the internship
+                        context.prepareSelection(cardView, internship, false);
+                    } else {
+                        //if wasn't already selected, then select the internship
+                        context.prepareSelection(cardView, internship, true);
+                    }
+
+                } else {
+                    //else a single click will take you to the internship information page
+                    Intent intent = new Intent(context, InternshipInformationActivity.class);
+                    //send the ID of the Internship you want to see information of
+                    intent.putExtra(InternshipTable.COLUMN_ID, internship.getInternshipID());
+                    context.startActivity(intent);
+
+                }
             }
         });
+
+        //set long click on card view to enable selection mode
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                CardView cardView = (CardView) holder.itemView;
+
+                //if the selection mode is off
+                if(!context.isSelectionMode) {
+                    //switch the selection mode on
+                    context.switchActionMode(true);
+                    //select the internship that was long clicked
+                    context.prepareSelection(cardView, internship, true);
+                } else {
+                    //else deselect internship if the selection mode was on
+                    context.prepareSelection(cardView, internship, false);
+                }
+
+                return true;
+            }
+        });
+
+        CardView cardView = (CardView) holder.itemView;
+
+        //on adapter refresh update the card backgrounds to back to default colors
+        if(internship.isSelected()) {
+            context.updateCardBackground(cardView, true);
+        } else {
+            context.updateCardBackground(cardView, false);
+        }
 
     }
 
